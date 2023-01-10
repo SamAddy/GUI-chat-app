a99422c0de253bb8ec5c59985dfe8da43e81b889
SamAddy <addisonsamuel70@yahoo.com>
2023-01-10 23:10:23 +0200

    Update the client and server to communicate

--- GUI-chat/client.py
+++ GUI-chat/client.py
@@ -1,8 +1,43 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"
-PORT = 1234
+HOST = '127.0.0.1'
+PORT = 65432
+
+
+def listen_for_messages_from_server(client):
+
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+        if message != "":
+            username = message.split("~")[0]
+            content = message.split("~")[1]
+        else:
+            print("Message received from client is empty")
+
+
+def send_message_to_server(client):
+
+    while 1:
+        message = input("Message: ")
+        if message != "":
+            client.sendall(message.encode())
+        else:
+            print("Empty message")
+            exit(0)
+
+
+def communicate_to_server(client):
+    username = input("Enter username: ")
+
+    if username != "":
+        client.sendall(username.encode())
+    else:
+        print("Username cannot be empty.")
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+    send_message_to_server(client)
 
 
 def main():
@@ -15,6 +50,8 @@ def main():
         print(f"Successfully connected to the server")
     except ConnectionError:
         print(f"Unable to connect to host {HOST} and port {PORT}")
+
+    communicate_to_server(client)
 
 
 if __name__ == '__main__':
--- GUI-chat/server.py
+++ GUI-chat/server.py
@@ -1,9 +1,66 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"
-PORT = "1234"
+HOST = '127.0.0.1'
+PORT = 65432
 LISTENER_LIMIT = 5
+# List of all connected clients
+active_clients = []
+
+
+def listen_for_messages(client, username):
+    """
+    This function listens for any upcoming messages
+
+    Param:
+    client
+    """
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+
+        if message != '':
+            # If message isn't empty then final message should be in the format username ~ message
+            # eg: Sam ~ Hello world!
+            final_message = username + "~" + message
+            send_message_to_all(final_message)
+        else:
+            print(f"The message sent from client {username} is empty.")
+
+
+def send_message_to_client(client, message):
+    """
+    This function forwards message to a specific client.
+    """
+    client.sendall(message.encode())
+
+
+def send_message_to_all(message):
+    """"
+    This function sends message to all active clients connected to the server.
+    """
+
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+
+def client_handler(client):
+    """
+    Server listens for client message
+    Contain username
+    """
+
+    while 1:
+        # Wait for any message from a specific client.
+        username = client.recv(2048).decode('utf-8')
+        if username != "":
+            active_clients.append((username, client))
+            notification = "SERVER~" + f"{username} just joined the chat."
+            send_message_to_client(notification)
+            break
+        else:
+            print("Client username is empty")
+    # Thread to get the socket to listen all the time.
+    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
 
 
 def main():
@@ -14,9 +71,9 @@ def main():
 
     try:
         server.bind((HOST, PORT))
-        print(f"Server is running on host {HOST} and port {PORT}")
+        print(f"Server is running on host {HOST}:{PORT}")
     except ConnectionError:
-        print(f"Unable to bind to host {HOST} and port {PORT}")
+        print(f"Unable to bind to host {HOST}:{PORT}")
 
     # Listening to server limit
     server.listen(LISTENER_LIMIT)
@@ -24,7 +81,9 @@ def main():
     # Keep listening to client connections
     while 1:
         client, address = server.accept()
-        print(f"Successfully connect to client {address[0]} {address[1]}")
+        print(f"Successfully connected to client {address[0]} {address[1]}")
+        # Thread is started when a client gets connected
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 
 if __name__ == '__main__':
